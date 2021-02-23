1bf56976eca80f1d39d4517aced1648d3d8af358
Emmapr123 <emmapriester@hotmail.com>
2021-02-23 15:25:11 +0000

    makes an array from 0 to number

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,13 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    'fizzbuzz'
-  elsif number % 3 == 0
-    'fizz'
-  elsif number % 5 == 0
-    'buzz'
-  end 
+  (1..number).to_a.map { |num|
+    if num % 3 == 0 && num % 5 == 0
+      'fizzbuzz'
+    elsif num % 3 == 0
+      'fizz'
+    elsif num % 5 == 0
+      'buzz'
+    else
+      num
+    end
+  }
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,12 +2,15 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq [1, 2, 'fizz']
   end
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(5)).to eq [1, 2, 'fizz', 4, 'buzz']
   end
   it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(15)).to eq [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz"]
+  end
+  it 'returns the number when passed 2' do
+    expect(fizzbuzz(2)).to eq [1, 2]
   end
 end
